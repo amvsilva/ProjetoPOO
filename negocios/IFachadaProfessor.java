d35190f8d3391eb97a77ae125c0c3ed7cb3ef504
Vitor Oliveira <vmoliveira91@gmail.com>
2019-05-09 15:20:33 -0300

    Finalizando as assinaturas da interface

--- negocios/IFachadaProfessor.java
+++ negocios/IFachadaProfessor.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public interface IFachadaProfessor {
   public Professor logarProfessor(String login, String senha) throws UsuarioNaoCadastradoException, SenhaInvalidaException;
   public boolean cadastrarProfessor(Professor professor) throws UsuarioJaCadastradoException;
-  public ArrayList<String> exibirTurmasProfessor(int professorId) throws SemTurmaCadastradaException;
-  public Turma exibirTurmaProfessor(String login);
-  public void atualizarNota(String loginProfessor, String loginAluno); 
-  // Continuar ...
+  public ArrayList<Turma> exibirTurmasProfessor(int professorId) throws SemTurmaCadastradaException;
+  public ArrayList<RendimentoEscolar> exibirNotasProfessor(int turmaId) throws SemAlunoMatriculadoException;
+  public boolean atualizarNotasProfessor(int alunoId, float nota1, float 2) throws NotaInvalidaException;
+  public boolean atualizarNotasTrabalhosProfessor(float[] notaTrabalhos) throws NotaInvalidaException;
+  public ArrayList<RendimentoEscolar> exibirListagemProfessor(int turmaId) throws SemAlunoMatriculadoException;
+  public ArrayList<Turma> exibirListagemTurmasDisponiveisProfessor(int professorId) throws SemTurmaCadastradaException;
+  public boolean associarTurmaProfessor(Professor professor);
 }
