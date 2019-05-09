f33cd08440f1fecc7ab6490d3e4ef9f2db274716
Vitor Oliveira <vmoliveira91@gmail.com>
2019-05-09 19:21:11 -0300

    Update IFachadaProfessor.java

--- negocios/IFachadaProfessor.java
+++ negocios/IFachadaProfessor.java
@@ -7,7 +7,7 @@ public interface IFachadaProfessor {
   public boolean cadastrarProfessor(Professor professor) throws UsuarioJaCadastradoException;
   public ArrayList<Turma> exibirTurmasProfessor(int professorId) throws SemTurmaCadastradaException;
   public ArrayList<RendimentoEscolar> exibirNotasProfessor(int turmaId) throws SemAlunoMatriculadoException;
-  public boolean atualizarNotasProfessor(int alunoId, float nota1, float 2) throws NotaInvalidaException;
+  public boolean atualizarNotasProfessor(int alunoId, float nota1, float nota2) throws NotaInvalidaException;
   public boolean atualizarNotasTrabalhosProfessor(float[] notaTrabalhos) throws NotaInvalidaException;
   public ArrayList<RendimentoEscolar> exibirListagemProfessor(int turmaId) throws SemAlunoMatriculadoException;
   public ArrayList<Turma> exibirListagemTurmasDisponiveisProfessor(int professorId) throws SemTurmaCadastradaException;
