1a92529ab92389cdb6a69e40f34f8b2065e70870
hivsonmatheus <50427583+hivsonmatheus@users.noreply.github.com>
2019-05-10 00:10:56 -0300

    Update IFachadaAluno.java

--- negocios/IFachadaAluno.java
+++ negocios/IFachadaAluno.java
@@ -2,9 +2,13 @@ package negocios;
 
 public interface IFachadaAluno {
 	
-	public void logarAluno(String nomeUsuario,String senha)throws UsuarioNaoCadastradoException, SenhaInvalidaException;
-	
-	public void cadastrarAluno(Aluno aluno)throws UsuarioJaCadastradoException;
-	public void[] exibirTurmasAluno(string nomeUsuario)throws SemTurmaCadastradaException;
+	public Aluno logarAluno(String login, String senha) throws UsuarioNaoCadastradoException, SenhaInvalidaException;
+  	public boolean cadastrarAluno(Aluno aluno) throws UsuarioJaCadastradoException;
+  	public ArrayList<Turma> exibirTurmasAluno(int alunoId) throws SemTurmaCadastradaException;
+  	public ArrayList<RendimentoEscolar> exibirNotasAluno(int turmaId) throws SemAlunoMatriculadoException;
+	public boolean adicionarTrabalho(RendimentoEscolar trabalhoNovo) throws SemEspacoParaNovoTrabalhoException;
+		//todo procurar saber se põe (RendimentoEscolar trabalhoNovo) ou (string[] trabalhoNovo)
+  	public ArrayList<Turma> exibirListagemTurmasDisponiveisAluno(int AlunoId) throws SemTurmaCadastradaException;
+  	public boolean associarTurmaAluno(Aluno aluno);
   
 }
