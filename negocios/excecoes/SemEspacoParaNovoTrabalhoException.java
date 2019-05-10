ad6d5cedef78d76f3c398ece831dd1df6d595859
hivsonmatheus <50427583+hivsonmatheus@users.noreply.github.com>
2019-05-10 00:10:38 -0300

    Create SemEspacoParaNovoTrabalhoException.java

---
+++ negocios/excecoes/SemEspacoParaNovoTrabalhoException.java
@@ -0,0 +1,7 @@
+package negocios.excecoes;
+
+public class SemEspacoParaNovoTrabalhoException extends Exception {
+  public SemEspacoParaNovoTrabalhoException(String mensagem) {
+    super(mensagem);
+  }
+}
